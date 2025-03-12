bb02a2b238bfa918ff9335f4b7a58d86f059b8bc
liwei5205 <42137610+liwei5205@users.noreply.github.com>
2025-03-12 19:45:10 +0800

    Update BaiduPanDiskConstant.java

--- baidupandiskapi/src/main/java/com/kirck/baidu/BaiduPanDiskConstant.java
+++ baidupandiskapi/src/main/java/com/kirck/baidu/BaiduPanDiskConstant.java
@@ -12,10 +12,7 @@ public class BaiduPanDiskConstant {
         String GET_USER_INFO = "https://pan.baidu.com/rest/2.0/xpan/nas";
     }
 
-    public interface BAIDU{
-        String CLINETID = "s4jSHeMSpsdnZGQC2eoeO49w";
-        String ACCESSTOKEN = "21.cea4e588326b46e0fe9bc6765cdb577f.2592000.1574744817.705906526-17622149";
-    }
+
 
     public interface CODE{
         int SUCCESS = 0;
